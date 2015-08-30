69a90773e32b694a5b65ba951eacf6b22b5b42b3
Vinh Nguyen <vnguyen.cheme@gmail.com>
2015-08-30 11:01:48 -0600

    finished mean tea

--- javascript/controller.js
+++ javascript/controller.js
@@ -8,20 +8,50 @@ app.controller("HomeController", ["$scope", "teas", function($scope, teas) {
     });
     $scope.teaData = data;
   });
+  } else {
+    $scope.teaData = teas.teaData;
   }
   $scope.addQuant = function (input) {
       var quantity = this.quantity;
     teas.teaData.forEach(function (item, index, array) {
       if (item.name === input) {
+        if (quantity === undefined) {
+        item.quantity = item.quantity + 1;
+        } else {
         item.quantity = item.quantity + parseInt(quantity);
+        }
       }
     });
   };
-
+  $scope.checkout = "empty";
+  $scope.checkoutLength = [];
+  $scope.checkoutTea = function (tea) {
+    for (var i = 0; i < $scope.checkoutLength.length; i ++) {
+      if ($scope.checkoutLength[i] == tea) {
+        return;
+      }
+    }
+    $scope.checkoutLength.push(tea);
+    $scope.checkout = "(" + $scope.checkoutLength.length + ")"
+  }
 }]);
 
 app.controller("CartController", ["$scope", "teas", function($scope, teas) {
   console.log(teas.teaData);
   $scope.showTea = teas.teaData;
+  $scope.saveTea = function (amount, id) {
+    for (var i = 0; i < teas.teaData.length; i ++) {
+      if (id === teas.teaData[i]._id) {
+        teas.teaData[i].quantity = amount;
+      }
+    }
+  };
+  $scope.removeTea = function (id) {
+    for (var i = 0; i < teas.teaData.length; i ++) {
+      if (id === teas.teaData[i]._id) {
+        teas.teaData[i].quantity = 0;
+      }
+    }
+  };
 
 }]);
--- javascript/filter.js
+++ javascript/filter.js
@@ -1,7 +1,17 @@
 app.filter("customCurrency", function () {
   return function (input) {
     var result = input/100;
-    return result
-
+    return result;
   };
 });
+
+app.filter("totalPrice", ["teas", function(teas) {
+  return function (input) {
+    var result = 0;
+    for (var i = 0; i < input.length; i ++) {
+      result += input[i].price * parseInt(input[i].quantity);
+    }
+    return result;
+  };
+}]);
+
